67049a62175bf082ae3f4c8d8406245fd463aee1
parinpond <parinpond.parinpond.lag@gmail>
2021-01-29 00:10:40 +0700

    feat: use async/await and return response format api

--- handler.js
+++ handler.js
@@ -4,54 +4,58 @@ var pg = require('pg');
 var DATABASE_URL = 'postgres://postgres:1234@localhost:5433/notes';
 var client = new pg.Client(DATABASE_URL);
 client.connect();
-
+function response_format(code,result){
+  const response       = {};
+  response.status_code = code;
+  response.body        = JSON.stringify(result);
+  return response;
+}
 module.exports.create = async (event) => {
-  const data =JSON.parse(event.body);
-  client.query("INSERT INTO public.cat(name) values('"+data.name+"') RETURNING *", function (err, result) {
-    if(err){
-      console.error(err);
-    }else{
-      console.log('The result is: ', result.rows[0]);
-    }
-  });
+  const data     = JSON.parse(event.body);
+  if(data)
+  try {
+    const result = await client.query("INSERT INTO public.cat(name) values('"+data.name+"') RETURNING *");
+    return response_format('200',result.rows[0]);
+  } catch(e) {
+    return response_format(e.code,e.name);
+  }
 };
 module.exports.getAll = async (event) => {
-  client.query("SELECT * FROM public.cat", (err, result) => {
-    if(err){
-      console.error(err);
-    }else{
-      console.log('The result is: ', result.rows);
-    }
-  });
+  try {
+    const result = await client.query("SELECT * FROM public.cat");
+    return response_format('200',result.rows);
+  } catch(e) {
+    return response_format(e.code,e.name);
+  }
 };
 module.exports.getOne = async (event) => {
   const id =event.pathParameters.id;
-  client.query("SELECT * FROM public.cat WHERE id="+id+"", (err, result) => {
-    if(err){
-      console.error(err);
-    }else{
-      console.log('The result is: ', result.rows);
+  if(id)
+    try { 
+      const result = await client.query("SELECT * FROM public.cat WHERE id="+id+"")
+      return response_format('200',result.rows);
+    } catch(e) {
+      return response_format(e.code,e.name);
     }
-  });
 };
 module.exports.update = async (event) => {
-  const id   =event.pathParameters.id;
-  const data =JSON.parse(event.body);
-  client.query("UPDATE public.cat SET name = '"+data.name+"' WHERE id="+id+" RETURNING *", (err, result) => {
-    if(err){
-      console.error(err);
-    }else{
-      console.log('The result is: ', result.rows);
-    }
-  });
+  const id   = event.pathParameters.id;
+  const data = JSON.parse(event.body);
+  if(id && data)
+  try { 
+    const result = await client.query("UPDATE public.cat SET name = '"+data.name+"' WHERE id="+id+" RETURNING *")
+    return response_format('200',result.rows);
+  } catch(e) {
+    return response_format(e.code,e.name);
+  }
 };
 module.exports.delete = async (event) => {
   const id =event.pathParameters.id;
-  client.query("DELETE FROM public.cat WHERE id="+id+" RETURNING *", (err, result) => {
-    if(err){
-      console.error(err);
-    }else{
-      console.log('The result is: ', result.rows);
-    }
-  });
+  if(id)
+  try { 
+    const result = await client.query("DELETE FROM public.cat WHERE id="+id+" RETURNING *")
+    return response_format('200',result.rows);
+  } catch(e) {
+    return response_format(e.code,e.name);
+  }
 };
